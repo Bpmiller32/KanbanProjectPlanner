d8485f112125fd1aed0080cae45fc82ba6a85446
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2025-01-06 22:36:43 -0800

    Added Calendar component, linked to Firebase
    - "Dot" indicator based calendar, clickable, connected to upcoming events.
    - Can add anonymous events
    - Prompt for adding named events
    
    - Calendar, events, dots, all connected to Firebase and real time update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
 import { Board } from "./Board";
+import { Calendar } from "./Calendar";
 
 function App() {
   return (
     <>
       <h2 className="px-6 py-4 text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">
+        Upcoming Events
+      </h2>
+      <Calendar />
+
+      <h2 className="px-6 py-4 text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">
         Project Priority
       </h2>
-
       <Board />
     </>
   );
